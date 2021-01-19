8afcc5c76bb972f5deb493774de9e897dc7d4dcc
morayl <sea.gang.moray@gmail.com>
2021-01-19 23:15:31 +0900

    setup用関数と、回答を回答者に見せる関数を作成

--- JustOne/JustOne.js
+++ JustOne/JustOne.js
@@ -4,9 +4,20 @@ function clear() {
   for (let i = 1; i <= PLAYER_COUNT; i++) {
     getSheetInstanceByName(`player${i}`).getRange("A3").setValue("")
   }
-  const answersRange = `B${ANSWER_RANGE_BASE}:B${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
-  const answersRangeValues = Array(PLAYER_COUNT).fill(0).map((_, i) => ["=player" + (i + 1) + "!A3"])
-  Logger.log(answersRangeValues)
-  Logger.log(answersRange)
+  const answersRange = `B${ANSWER_RANGE_BASE}:C${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
+  const answersRangeValues = Array(PLAYER_COUNT).fill(0).map((_, i) => ["=player" + (i + 1) + "!A3","=player" + (i + 1) + "!A3"])
   getSheetInstanceByName("回答合わせ").getRange(answersRange).setValues(answersRangeValues)
+}
+
+function showAnswers(){
+  const answersRange = `B${ANSWER_RANGE_BASE}:B${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
+  const answersRangeValues = Array(PLAYER_COUNT).fill(0).map((_, i) => ["=回答合わせ!C" + (i + ANSWER_RANGE_BASE)])
+  getSheetInstanceByName("回答者").getRange(answersRange).setValues(answersRangeValues)
+}
+
+function setup(){
+  const namesRange = `A${ANSWER_RANGE_BASE}:A${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
+  const namesRangeValues = Array(PLAYER_COUNT).fill(0).map((_, i) => ["=player" + (i + 1) + "!A2"])
+  getSheetInstanceByName("回答合わせ").getRange(namesRange).setValues(namesRangeValues)
+  getSheetInstanceByName("回答者").getRange(namesRange).setValues(namesRangeValues)
 }
