ab3805dee9a9a79e78ad348d949db49e0cc8dad9
morayl <sea.gang.moray@gmail.com>
2021-01-21 22:24:43 +0900

    出題者ルーレットを追加

--- JustOne/JustOne.js
+++ JustOne/JustOne.js
@@ -5,9 +5,11 @@ const ANSWERER_ANSWER_RANGE = `B${ANSWER_RANGE_BASE}:B${ANSWER_RANGE_BASE + PLAY
 const DISTINCT_ANSWERS_RANGE = `B${ANSWER_RANGE_BASE}:B${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
 const ANSWER_SHEET_NAMES_RANGE = `A${ANSWER_RANGE_BASE}:A${ANSWER_RANGE_BASE + PLAYER_COUNT - 1}`
 const PLAYER_SHEET_NAME_ANSWER = "A2:A3"
+const THEME_SHEET_QUESTIONER = "D2"
 
 const SHEET_NAME_ANSWER_MERGE = "回答合わせ"
 const SHEET_NAME_ANSERER = "回答者"
+const SHEET_NAME_THEME = "お題"
 
 const SETUP_NAME = "名前はここ"
 const SETUP_ANSWER = "回答はここ"
@@ -21,6 +23,7 @@ function clear() {
   const answererRange = ANSWERER_ANSWER_RANGE
   const answererRangeValues = Array(PLAYER_COUNT).fill(0).map((_, i) => [""])
   getSheetInstanceByName(SHEET_NAME_ANSERER).getRange(answererRange).setValues(answererRangeValues)
+  getSheetInstanceByName(SHEET_NAME_THEME).getRange(THEME_SHEET_QUESTIONER).setValue("")
 }
 
 function showAnswers() {
@@ -37,4 +40,11 @@ function setup() {
   for (let i = 1; i <= PLAYER_COUNT; i++) {
     getSheetInstanceByName(`player${i}`).getRange(PLAYER_SHEET_NAME_ANSWER).setValues(playerNameAnswerValues)
   }
+}
+
+function randomQuestioner() {
+  const min = 0
+  const maxLess = PLAYER_COUNT
+  const number = Math.floor(Math.random() * (maxLess - min) + min)
+  getSheetInstanceByName(SHEET_NAME_THEME).getRange(THEME_SHEET_QUESTIONER).setValue(`=player${number + 1}!A2`)
 }
