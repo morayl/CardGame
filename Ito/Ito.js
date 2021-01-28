d01a3f65813a9f9adf366959031fe1ed3b7ff81f
morayl <sea.gang.moray@gmail.com>
2021-01-28 16:35:20 +0900

    回答を切り取り→貼り付けすると管理シートでREFエラーが起きる問題をIndrect関数で解決

--- Ito/Ito.js
+++ Ito/Ito.js
@@ -4,9 +4,10 @@ const SETUP_PLAYER_RANGE = "A1:C4"
 const SETUP_PLAYER_RANGE_VALUES = [["↓セルA2↓に名前を入れてね", "", "↓場のカード↓"], ["名前はここ", "", `='${SHEET_NAME_MASTER}'!$D$2`], ["", "手札", "↓出すカード↓"], ["", "", "ここに手札を貼り付け"]]
 const CELL_PLAYER_NAME = "$A$2"
 const CELL_PLAYER_ANSWER = "$C$4"
-const CELL_MASTER_CARD_COUNT = "$D$3"
+const CELL_MASTER_CARD_COUNT = "$E$3"
 const CELL_PLAYER_CARDS_COLUMN = "$B"
 const CELL_PLAYER_CARDS_ROW = 4
+const ROW_MASTER_PLAYER_START = 2
 
 function setupCards() {
   const cardCount = getSheetInstanceByName(SHEET_NAME_MASTER).getRange(CELL_MASTER_CARD_COUNT).getValue()
@@ -14,13 +15,13 @@ function setupCards() {
   Logger.log(cards)
   for (let i = 1; i <= PLAYER_COUNT; i++) {
     const playerCards = []
-    for (let i = 0; i <= cardCount; i++) {
+    for (let i = 0; i <= cardCount - 1; i++) {
       const rand = randomNumber(0, cards.length)
       const target = cards.splice(rand, 1)
       playerCards.push(target)
     }
     getSheetInstanceByName(`player${i}`)
-      .getRange(`${CELL_PLAYER_CARDS_COLUMN + CELL_PLAYER_CARDS_ROW}:${CELL_PLAYER_CARDS_COLUMN}${CELL_PLAYER_CARDS_ROW + cardCount}`)
+      .getRange(`${CELL_PLAYER_CARDS_COLUMN + CELL_PLAYER_CARDS_ROW}:${CELL_PLAYER_CARDS_COLUMN}${CELL_PLAYER_CARDS_ROW + cardCount - 1}`)
       .setValues(playerCards)
   }
 }
@@ -41,7 +42,7 @@ function setupPlayerSheet() {
 function setupMasterSheet() {
   let rows = []
   for (let i = 1; i <= PLAYER_COUNT; i++) {
-    rows.push([`=player${i}!${CELL_PLAYER_NAME}`, `=player${i}!${CELL_PLAYER_ANSWER}`])
+    rows.push([`=player${i}!${CELL_PLAYER_NAME}`, `=INDIRECT(C${ROW_MASTER_PLAYER_START + i - 1}&"!C4")`, `player${i}`])
   }
-  getSheetInstanceByName(SHEET_NAME_MASTER).getRange(`A2:B${PLAYER_COUNT + 1}`).setValues(rows)
+  getSheetInstanceByName(SHEET_NAME_MASTER).getRange(`A2:C${PLAYER_COUNT + 1}`).setValues(rows)
 }
