794bdd90e2ee5333a8f555ecfd63621d0b077513
morayl <sea.gang.moray@gmail.com>
2021-02-06 16:21:19 +0900

    sheetをinsertまたは取得する関数をUtil化

--- Common/Utils.js
+++ Common/Utils.js
@@ -28,3 +28,17 @@ function notUndefined(value) {
 function randomNumber(min, maxLess) {
   return Math.floor(Math.random() * (maxLess - min) + min)
 }
+
+function insertOrGetSheet(sheetName, index) {
+  let sheet
+  try {
+    if (notUndefined(index)) {
+      sheet = SpreadsheetApp.getActive().insertSheet(sheetName, index)
+    } else {
+      sheet = SpreadsheetApp.getActive().insertSheet(sheetName)
+    }
+  } catch (e) {
+    sheet = getSheetInstanceByName(sheetName)
+  }
+  return sheet
+}
